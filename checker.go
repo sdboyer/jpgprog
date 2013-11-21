0ee44ebc4cd9f51ba71a4e34e8024d5003723bce
Sam Boyer <tech@samboyer.org>
2013-11-21 12:49:36 -0500

    gofmt ftw

--- checker.go
+++ checker.go
@@ -18,16 +18,14 @@ type ImageResult struct {
 type ImageResultSet map[string]bool
 
 const (
-  sof0Marker = 0xc0
-  sof2Marker = 0xc2
+	sof0Marker = 0xc0
+	sof2Marker = 0xc2
 )
 
 // Given a string of HTML, searches it for jpg references and determines
 // whether referenced images are progressive or not for all the images.
-func GetImageResults(html string) (result ImageResultSet, err error) {
-	reader := string.NewReader(document)
-
-	if doc, e = goquery.NewDocumentFromReader(string.NewReader(html)); e != nil {
+func GetImageResults(body io.ReadCloser) (result ImageResultSet, err error) {
+	if doc, e = goquery.NewDocumentFromReader(body); e != nil {
 		panic(e.Error())
 	}
 
@@ -56,63 +54,63 @@ func GetImageResults(html string) (result ImageResultSet, err error) {
 
 // Given an io reader (which we assume contains jpeg data), determine if the
 // jpeg represented therein is progressive.
-func IsJpgProgressive(r io.Reader) (bool, error){
-  var cur [512]byte
+func IsJpgProgressive(r io.Reader) (bool, error) {
+	var cur [512]byte
 
-  // process initial SOI marker
-  _, err := io.ReadFull(r, cur[0:2])
-  if err != nil {
-    return nil, err
-  }
-  if marker[0] != 0xff || marker[1] != 0xd8 {
-    return nil, jpeg.FormatError("missing SOI marker")
-  }
+	// process initial SOI marker
+	_, err := io.ReadFull(r, cur[0:2])
+	if err != nil {
+		return nil, err
+	}
+	if marker[0] != 0xff || marker[1] != 0xd8 {
+		return nil, jpeg.FormatError("missing SOI marker")
+	}
 
-  // now, scan through the body for a progressive marker
-  for {
-    _, err := io.ReadFull(r, cur[0:2])
-    if err != nil {
-      return nil, err
-    }
+	// now, scan through the body for a progressive marker
+	for {
+		_, err := io.ReadFull(r, cur[0:2])
+		if err != nil {
+			return nil, err
+		}
 
-    for cur[0] != 0xff {
-      cur[0] = cur[1]
-      cur[1], err = r.ReadByte()
-      if err != nil {
-        return nil, err
-      }
-    }
-    marker := cur[1]
+		for cur[0] != 0xff {
+			cur[0] = cur[1]
+			cur[1], err = r.ReadByte()
+			if err != nil {
+				return nil, err
+			}
+		}
+		marker := cur[1]
 
-    if marker == 0x00 {
-      // "\xff\x00" is extraneous data, according to JPEG spec
-      continue;
-    }
+		if marker == 0x00 {
+			// "\xff\x00" is extraneous data, according to JPEG spec
+			continue
+		}
 
-    for marker == 0xff {
-      // spec allows as many fill bytes as desired with val "\xff"
-      marker, err = r.ReadByte()
-      if err != nil {
-        return nil, err
-      }
-    }
+		for marker == 0xff {
+			// spec allows as many fill bytes as desired with val "\xff"
+			marker, err = r.ReadByte()
+			if err != nil {
+				return nil, err
+			}
+		}
 
-    if 0xd0 <= marker && marker <= 0xd7 {
-      // weird shit i don't grok about reset markers
-      continue;
-    }
+		if 0xd0 <= marker && marker <= 0xd7 {
+			// weird shit i don't grok about reset markers
+			continue
+		}
 
-    // we know we're on a marker that has a length payload - figure
-    // out how long it is (less the 2 bytes for the marker)
-    _, err = io.ReadFull(r, cur[0:2])
-    if err != nil {
-      return nil, err
-    }
+		// we know we're on a marker that has a length payload - figure
+		// out how long it is (less the 2 bytes for the marker)
+		_, err = io.ReadFull(r, cur[0:2])
+		if err != nil {
+			return nil, err
+		}
 
-    n := int(cur[0])<<8 + int(cur[1]) - 2
+		n := int(cur[0])<<8 + int(cur[1]) - 2
 
-    if marker == sof0Marker || marker == sof2Marker {
-      return marker == sof2marker, nil
-    }
-  }
+		if marker == sof0Marker || marker == sof2Marker {
+			return marker == sof2marker, nil
+		}
+	}
 }
