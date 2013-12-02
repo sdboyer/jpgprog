863bd9b53ac5d6b6091cee0fc4b84716195eefc4
Sam Boyer <tech@samboyer.org>
2013-12-02 17:15:06 -0500

    Now, make a binary.

---
+++ jpgprog.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sdboyer/jpgprog/lib"
+    "github.com/codegangsta/cli"
+	"net/http"
+    "os"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "jpgprog"
+  app.Usage = "Specify a url, and jpegprog will report on whether any jpegs contained therein are progressive."
+  app.Action = func(c *cli.Context) {
+    for _, url := range c.Args() {
+      doFromUrl(url)
+    }
+  }
+
+  app.Run(os.Args)
+}
+
+func doFromUrl(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	results, err := jpgprog.GetImageResults(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+    for url, progressive := range results {
+      fmt.Println(progressive, url)
+    }
+}
+
